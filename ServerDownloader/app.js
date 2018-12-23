eef37a50720bb8f1b0eba1ea1304179b06f08640
rrdm23 <rrdm23998@hotmail.com>
2018-12-23 15:40:46 -0600

    Documentación de servidor y funciones de BD

--- ServerDownloader/app.js
+++ ServerDownloader/app.js
@@ -1,3 +1,4 @@
+// Importing variables
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -10,7 +11,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -20,10 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route settings
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
+// Route that builds the taxonomy tree, given a tax and a name.
+// Potentially needs a year to be added to the search.
 app.post('/search', function(req, res) {
   //res.send('ab?cd');
   let tax = req.query.tax;
@@ -35,18 +38,18 @@ app.post('/search', function(req, res) {
 });
 
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
 });
